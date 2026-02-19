cf717861918de170495869426a8c59abc32e31dd
lms-30 <lahinirikomarasylvain30@gmail.com>
2026-02-19 14:46:16 +0300

    Delete scripts directory

--- scripts/install-trivy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installation de Trivy..."
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh \
-    | sh -s -- -b /usr/local/bin
-trivy --version
-echo "Trivy installé avec succès ✅"
